063a7275ca5e390e35decf38d0c68e446ac565ee
Davis Vaughan <davis@posit.co>
2024-04-20 09:16:28 -0400

    Write initial `query_matches()` and `query_captures()` docs

--- R/query.R
+++ R/query.R
@@ -18,9 +18,98 @@ query <- function(language, source) {
   new_query(pointer, capture_names, pattern_predicates, source, language)
 }
 
-# TODO: Document that you need to escape `#match?` regex strings with double `\\`
-# and the easiest way is with raw strings, or reading from a query.scm file.
+#' Query matches and captures
+#' 
+#' @description
+#' These two functions execute a query on a given `node`, and return the
+#' captures of the query for further use. Both functions return the same
+#' information, just structured differently depending on your use case. 
+#' 
+#' - `query_matches()` returns the captures first grouped by _predicate_, and
+#'   further grouped by _match_ within each predicate. This is useful if you
+#'   include multiple predicates in your query or if you expect multiple
+#'   captures per match.
+#' 
+#' - `query_captures()` returns a flat list of captures ordered by their node
+#'   location in the original text. This is normally the easiest structure to
+#'   use if you have a single predicate with one capture per match.
+#' 
+#' Both also return the name of the capture, i.e. the `@name` you specified in
+#' your query.
+#' 
+#' @section Predicates: 
+#' 
+#' There are 3 core types of predicates supported:
+#' 
+#' - `#eq? @capture "string"`
+#' - `#eq? @capture1 @capture2`
+#' - `#match? @capture "regex"`
+#' 
+#' Each of these predicates can also be inverted with a `not-` prefix, i.e.
+#' `#not-eq?` and `#not-match?`.
+#' 
+#' ### String double quotes
+#' 
+#' The underlying tree-sitter predicate parser requires that strings supplied
+#' in a query must use double quotes, i.e. `"string"` not `'string'`. If you
+#' try and use single quotes, you will get a query error.
+#' 
+#' ### `#match?` regex
+#' 
+#' The regex support provided by `#match?` is powered by [grepl()].
+#' 
+#' Escapes are a little tricky to get right within these match regex strings.
+#' To use something like `\s` in the regex string, you need the literal text
+#' `\\s` to appear in the string to tell the tree-sitter regex engine to escape
+#' the backslash so you end up with just `\s` in the captured string. This
+#' requires putting two literal backslash characters in the R string itself,
+#' which can be accomplished with either `"\\\\s"` or using a raw string like
+#' `r'["\\s"]'` which is typically a little easier. You can also write your
+#' queries in a separate file (typically called `queries.scm`) and read them
+#' into R, which is also a little more straightforward because you can just
+#' write something like `(#match? @id "^\\s$")` and that will be read in
+#' correctly.
+#' 
+#' @inheritParams rlang::args_dots_empty
+#' 
+#' @param x `[tree_sitter_query]`
+#' 
+#'   A query.
+#' 
+#' @param node `[tree_sitter_node]`
+#' 
+#'   A node to run the query over.
+#' 
+#' @param range `[tree_sitter_range / NULL]`
+#' 
+#'   An optional range to restrict the query to.
+#' 
+#' @name query-matches-and-captures
+#' 
+#' @examplesIf rlang::is_installed("treesitter.r")
+#' text <- "
+#' foo + b + a + ab
+#' and(a)
+#' "
+#' 
+#' source <- "(identifier) @id"
+#' 
+#' language <- treesitter.r::language()
+#' 
+#' query <- query(language, source)
+#' 
+#' node <- language |>
+#'   parser() |>
+#'   parser_parse(text) |>
+#'   tree_root_node()
+#' 
+#' # A flat ordered list of captures, that's most useful here since
+#' # we only have 1 pattern!
+#' captures <- query_captures(query, node)
+#' captures$node
+NULL
 
+#' @rdname query-matches-and-captures
 #' @export
 query_matches <- function(x, node, ..., range = NULL) {
   check_dots_empty0(...)
@@ -75,6 +164,7 @@ query_matches <- function(x, node, ..., range = NULL) {
   out
 }
 
+#' @rdname query-matches-and-captures
 #' @export
 query_captures <- function(x, node, ..., range = NULL) {
   check_dots_empty0(...)
