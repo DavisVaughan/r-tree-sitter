b5c91a4138c0ee61722e646813155da98d657d7d
Jenny Bryan <jenny.f.bryan@gmail.com>
2024-04-29 15:47:06 -0700

    Add a query for describe() + it()

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -11,6 +11,15 @@ get_captures <- function(code_source, query_source) {
   query_captures(query, node)
 }
 
+get_matches <- function(code_source, query_source) {
+  language <- r()
+  parser <- parser(language)
+  tree <- parser_parse(parser, code_source)
+  node <- tree_root_node(tree)
+  query <- query(language, query_source)
+  query_matches(query, node)[[1]]
+}
+
 expect_top_level <- function(node) {
   expect_equal(
     node_type(node_parent(node)),
